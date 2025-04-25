68135a6ee5bd1e85295acda24c21da89dc84341d
bhp0507 <bhp0507@koseongnam.com>
2025-04-25 14:13:36 +0900

    React Portfolio Upload

---
+++ src/data/user.js
@@ -0,0 +1,84 @@
+const INFO = {
+	main: {
+		title: "Reactfolio by truethari",
+		name: "Tharindu N.",
+		email: "mail@tharindu.dev",
+		logo: "../logo.png",
+	},
+
+	socials: {
+		twitter: "https://twitter.com/",
+		github: "https://github.com/",
+		linkedin: "https://linkedin.com/",
+		instagram: "https://instagram.com/",
+		stackoverflow: "https://stackoverflow.com/",
+		facebook: "https://facebook.com/",
+	},
+
+	homepage: {
+		title: "소통과 도전으로 성장하는 풀스택, AI 개발자 박병현 입니다.",
+		description:
+			"저는 컴퓨터 공학을 전공하였고, AI 부트캠프를 통해 AI 역량을 길렀습니다. Springboot를 이용한 웹 어플리케이션 프로젝트 백엔드 구축 경험을 가지고 있으며, React Framework를 사용하여 웹페이지 구현 경험을 가지고 있습니다. AWS, GCP를 사용한 클라우드 개발 환경 경험이 있으며, AI 역량을 갖춘 전문가가 되기 위해 끊임없이 성장하고 도전하는 개발자입니다.",
+	},
+
+	about: {
+		title: "I'm Park ByeungHyeon. I live in Seoul, Korea, where I design the future.",
+		description:
+			"I've worked on a variety of projects over the years and I'm proud of the progress I've made. Many of these projects are open-source and available for others to explore and contribute to. If you're interested in any of the projects I've worked on, please feel free to check out the code and suggest any improvements or enhancements you might have in mind. Collaborating with others is a great way to learn and grow, and I'm always open to new ideas and feedback.",
+	},
+
+	articles: {
+		title: "I'm passionate about pushing the boundaries of what's possible and inspiring the next generation of innovators.",
+		description:
+			"Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
+	},
+
+	projects: [
+		{
+			title: "Project 1",
+			description:
+				"Smart WMS 시스템 프로젝트에서 인증/인가 기능, RAG ChatBot, 웹 어플리케이션 백엔드 구축을 하였습니다.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/python/python.png",
+			linkText: "View Project",
+			link: "https://github.com/Park-ByeongHyeon/human_project",
+		},
+
+		{
+			title: "Project 2",
+			description:
+				"긴급 신고 자동 접수 시스템 : 신고잉 프로젝트에서 웹 페이지 구현, STT/TTS 기능 구현을 하였습니다.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/python/python.png",
+			linkText: "View Project",
+			link: "https://github.com/AIVLE-16/client",
+		},
+
+		{
+			title: "Project 3",
+			description:
+				"캡스톤 프로젝트로 진행한 얼굴 인식 기술을 이용한 신원 확인 시스템 프로젝트로 대학생 논문경진대회 수상을 하였습니다.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/python/python.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "Project 4",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "Project 5",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+	],
+};
+
+export default INFO;
